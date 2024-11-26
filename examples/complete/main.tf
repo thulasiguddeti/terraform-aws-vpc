d187367b38f8f640ee1e5b1d41d5b8c91605d058
thulasiguddeti <thulasiguddeti02@gmail.com>
2024-11-26 16:04:55 +0530

    aws-vpc

---
+++ examples/complete/main.tf
@@ -0,0 +1,20 @@
+module "roboshop" {
+  #source = "../terraform-aws-vpc"
+  source = "git::https://github.com/thulasiguddeti/terraform-aws-vpc.git?ref=main"
+  project_name = var.project_name
+  environment = var.environment
+  common_tags = var.common_tags
+  vpc_tags = var.vpc_tags
+
+  # public subnet
+  public_subnets_cidr = var.public_subnets_cidr
+
+  # private subnet
+  private_subnets_cidr = var.private_subnets_cidr
+
+  # database subnet 
+  database_subnets_cidr = var.database_subnets_cidr
+   
+  #perring
+  is_peering_required = var.is_peering_required
+}
